fefed5a0718a72c0cb6b2a85a2b9720d7ce29c0b
권지현 <dial2825@naver.com>
2021-04-03 01:17:20 +0900

    no message

---
+++ TCPServer6/TCPServer6/TCPServer6.cpp
@@ -0,0 +1,119 @@
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SERVERPORT 9000 
+#define BUFSIZE 512
+
+//소켓함수 오류 출력 후 종료
+void err_quit(const char* msg) {
+	LPVOID IpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&IpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)IpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(IpMsgBuf);
+	exit(1);
+}
+
+//소켓함수 오류 출력 
+void err_display(const char* msg) {
+	LPVOID IpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&IpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char*)IpMsgBuf);
+	LocalFree(IpMsgBuf);
+}
+
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	//윈속 초기화 
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	//socket()
+	SOCKET listen_sock = socket(AF_INET6, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+	//bind()
+	SOCKADDR_IN6 serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin6_family = AF_INET6;
+	serveraddr.sin6_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin6_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	//listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	//데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
+
+	while (1) {
+		//accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET) {
+			err_display("accept()");
+			break;
+		}
+
+		//접속한 클라이언트 정보 출력
+		printf("\n [ TCP 서버 ] 클라이언트 접속 IP 주소=%s, 포트번호 = %d\n",
+			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+		//클라이언트와 데이터 통신
+		while (1) {
+			//데이터 받기
+			retval = recv(client_sock, buf, BUFSIZE, 0);
+			if (retval == SOCKET_ERROR) {
+				err_display("recv()");
+				break;
+			}
+			else if (retval == 0)
+				break;
+
+			//받은 데이터 출력
+			buf[retval] = '\0';
+			printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr),
+				ntohs(clientaddr.sin_port), buf);
+
+			//데이터 보내기 
+			retval = send(client_sock, buf, retval, 0);
+			if (retval == SOCKET_ERROR) {
+				err_display("send()");
+				break;
+			}
+		}
+
+		//closersocket()
+		closesocket(client_sock);
+		printf("[TCP 서버] 클라이언트 종료:IP 주소 = %s, 포트번호 =%d\n",
+			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+	}
+
+	//closesocket()
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+
+}
+
